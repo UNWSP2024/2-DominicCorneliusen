6c940c95e22533fb1582296d04bca4f056a9af53
DominicCorneliusen <dmcorn.004@icloud.com>
2026-01-29 11:22:45 -0600

    Add files via upload

---
+++ Average_Age/main.py
@@ -0,0 +1,14 @@
+#Title: Personal Information
+#Author: Dominic Corneliusen
+#Date last modified: 1/29/26
+
+#Variables
+Carter = 17
+Jaycie = 17
+Malechi = 17
+Kendyl = 18
+Jayden = 17
+Average_Age = (Carter + Jaycie + Malechi + Kendyl + Jayden) / 5
+
+#Print
+print(Average_Age)
