11d888b7868a82ba1605587a19034e8cd18e38f7
DominicCorneliusen <dmcorn.004@icloud.com>
2026-01-29 15:53:11 -0600

    Add files via upload

---
+++ TempConversion/main.py
@@ -0,0 +1,10 @@
+#Title: Temperature Conversion
+#Author: Dominic Corneliusen
+#Date Last Edited: 1/29/26
+
+#Variables/User Input
+Degrees_Celsius = float(input("Enter the temperature in ºCelsius: "))
+Degrees_Fahrenheit = ((9 / 5) * Degrees_Celsius) + 32
+
+#Print
+print(Degrees_Celsius, "ºCelsius is", Degrees_Fahrenheit, "ºFahrenheit")
